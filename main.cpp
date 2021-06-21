0191df68bdc1d82ddfdf5affc62fd220c1e98295
nonso <chinonsojacobs@gmail.com>
2021-06-21 18:21:10 +0100

    cpp project

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include<conio.h>
+#include<ctime>
+#include<string.h>
+#include<cstring>
+using namespace std;
+
+string name[] = {};
+string classes[] = {};
+string module[4] = {"MD4005", "MD4003", "MD4007", "MD4004"};
+string lecturers[7] = {"Prof John Smith", "Prof Emma Barkley", "Prof Harry Porter", "Prof Tom Hardy", "Prof Daniel Ezra", "Prof Emma Stone", "Prof Zendaya Winterfield"};
+string room[7] = {"Room12", "Room13", "Room14", "Room15", "Room16", "Room19", "Room17"};
+string day[5] = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday"};
+int Time[7] = { 10,11,12,1,2,3,4 };
+
+void timeT(){
+	cout <<"Enter Student Name: "<< endl;
+	cin >> name[n];
+	cout <<"Enter Class: "<< endl;
+//	cin >>classes;
+//	cout <<"Select Module";
+//	cout << "1. " + module[0] <<endl;
+//	cout << "2. " + module[1] <<endl;
+//	cout << "3. " + module[2] <<endl;
+//	cout << "4. " + module[3] <<endl;
+//	cin >>moduleno;
+//	switch(moduleno){
+//		case 1: 
+//	}
+}
+
+int main(int argc, char** argv) {
+	timeT();
+	return 0;
+}
